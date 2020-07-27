5e9f3aa8eedcc9b2b26d43f53e894ce2dfdaac5b
singsmiya <>
2020-07-27 21:15:22 +0800

    first

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -5,6 +5,12 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
+    let fileMiddleware = FileMiddleware(
+        publicDirectory: app.directory.publicDirectory
+    )
+    app.middleware.use(fileMiddleware)
+    
     // register routes
     try routes(app)
+    print(app.routes.all)
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,22 @@ func routes(_ app: Application) throws {
     app.get("hello") { req -> String in
         return "Hello, world!"
     }
+    
+    app.get("hello", ":name") { req -> String in
+        let name = req.parameters.get("name")!
+        return "Hello, \(name)!"
+    }
+    
+    app.get("foo", "bar", "baz") { req in
+        return "foo!"
+    }
+    
+    app.get("number", ":x") { req -> String in
+        guard let int = req.parameters.get("x", as: Int.self) else {
+            throw Abort(.badRequest)
+        }
+        return "\(int) is a great number"
+    }
+    
+    
 }
